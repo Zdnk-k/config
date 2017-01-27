2a4e504091dcbcf5aa94c323527c1221102b953b
xklain <xklain@fi.muni.cz>
2017-01-27 21:48:00 +0100

    powershell profile

---
+++ profile.ps1
@@ -0,0 +1,9 @@
+function cdCode {Set-Location D:\CODE\}
+function cdJava {Set-Location D:\CODE\Java}
+function cdJavascript {Set-Location D:\CODE\javascript}
+function cdPython {Set-Location D:\CODE\Python}
+Set-Alias cdc cdCode
+Set-Alias cdpy cdPython
+Set-Alias cdj cdJava
+Set-Alias cdjs cdJavascript
+
